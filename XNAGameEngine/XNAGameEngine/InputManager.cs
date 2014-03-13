e2599f38a4307030c3327cc776e921419936836d
macv101 <michaelvillar0012@gmail.com>
2014-03-13 01:23:26 -0700

    Added Input Files

---
+++ XNAGameEngine/XNAGameEngine/InputManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+
+namespace XNAGameEngine
+{
+     class InputManager
+    {
+        private List<Event> _EVENT;
+        private GameMouse _mouse;
+        
+        public Event this[string eventName]
+        { 
+            get
+            { 
+                    return _EVENT.Find((Event a)=>{return a._name == eventName;});
+            }
+        }
+        public InputManager(GameInterface gi)
+        {
+            _mouse = new GameMouse(gi, "Assets/Ship");
+            _EVENT = new List<Event>();
+        }
+        
+        public void AddEvent (string eventName)
+        {
+            _EVENT.Add(new Event(this, eventName));
+        }
+
+        public void Draw()
+        {
+            _mouse.Draw();
+        }
+        public void Update(GameTime gameTime)
+        {
+            KeyboardState kbState = Keyboard.GetState();
+            Mouse.GetState();
+
+            foreach (Event a in _EVENT)
+                a.Update(kbState);
+
+            _mouse.Update(gameTime);
+        }
+
+        
+    }
+}
