7febd01518a445be0edfd59bab8ea6bbf9651e2c
Flowers61 <flores.eric94@gmail.com>
2014-03-20 15:35:33 -0700

    GameScreen.cs

--- XNAGameEngine/XNAGameEngine/InputManager.cs
+++ XNAGameEngine/XNAGameEngine/InputManager.cs
@@ -10,29 +10,29 @@ using Microsoft.Xna.Framework.Input;
 
 namespace XNAGameEngine
 {
-     class InputManager
+    class InputManager
     {
         private List<InputEvent> _EVENT;
-        public  GameMouse _mouse;
+        public GameMouse _mouse;
 
         public InputEvent this[string eventName]
-        { 
+        {
             get
-            { 
-                    return _EVENT.Find((InputEvent a)=>{return a._Name == eventName;});
+            {
+                return _EVENT.Find((InputEvent a) => { return a._Name == eventName; });
             }
         }
-      
+
         public InputManager(GameInterface gi)
         {
-            _mouse = new GameMouse(gi, "Assets/Ship");
+            _mouse = new GameMouse(gi, "Assets/harpoon mouse");
             _EVENT = new List<InputEvent>();
         }
-       
-        public void AddEvent (string eventName)
+
+        public void AddEvent(string eventName)
         {
             _EVENT.Add(new InputEvent(this, eventName));
-        }      
+        }
 
         public void Draw()
         {
@@ -48,6 +48,41 @@ namespace XNAGameEngine
 
             _mouse.Update(gameTime);
         }
-       
     }
+
+    class InputEvent
+    {
+        public string _Name;
+
+        List<Keys> _KEYBOARD = new List<Keys>();
+        InputManager _Im = null;
+        bool _HeldPressed = false;
+        bool _TapPressed = false;
+
+        public bool IsDown { get { return _HeldPressed; } }
+        public bool IsTapped { get { return (_HeldPressed) && (!_TapPressed); } }
+
+        public InputEvent(InputManager im, string name)
+        {
+            _Im = im;
+            _Name = name;
+        }
+
+        public void Add(Keys key)
+        {
+            if (!_KEYBOARD.Contains(key))
+                _KEYBOARD.Add(key);
+        }
+
+        internal void Update(KeyboardState kbState)
+        {
+            _TapPressed = _HeldPressed;
+            _HeldPressed = false;
+
+            foreach (Keys k in _KEYBOARD)
+                if (kbState.IsKeyDown(k))
+                    _HeldPressed = true;
+        }
+    }
+
 }
