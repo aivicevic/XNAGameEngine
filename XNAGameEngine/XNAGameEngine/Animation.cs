b47a4f90a1ddcef6b3d14841f6a96b4320f4037e
Flowers61 <flores.eric94@gmail.com>
2014-03-23 12:39:01 -0700

    Edited Animation.cs

--- XNAGameEngine/XNAGameEngine/Animation.cs
+++ XNAGameEngine/XNAGameEngine/Animation.cs
@@ -14,11 +14,14 @@ namespace XNAGameEngine
     {
         #region Private Properties
         private Vector2 _currentFrame;
-        private Vector2 _numFrames;
+        private Vector2 _numFrames;     
         private int _frameCounter;
         private int _switchFrame;
         private int _frameWidth, _frameHeight;
         private Rectangle _frameRect;
+        private Vector2 _startFrame;
+        private Vector2 _endFrame;
+        private bool _continuouse;
         #endregion
 
         #region Public Accessors
@@ -29,18 +32,23 @@ namespace XNAGameEngine
         public Vector2 getPivot { get { return new Vector2(_frameWidth/2, _frameHeight / 2); } }
         public int frameWidth { get { return _frameWidth; } }
         public int frameHeight { get { return _frameHeight; } }
+        public Vector2 startFrame { get { return _startFrame; } set { _startFrame = value; } }
+        public Vector2 endFrame { get { return _endFrame; } set { _endFrame = value; } }
+        public bool continuouse { get { return _continuouse} set { _continuouse = value;} }
         #endregion
 
         #region Public Constructor
-        public Animation(Vector2 numFrames, int rate, Sprite sprite)
+        public Animation(Vector2 numFrames, Vector2 startFrame, Vector2 endFrame, int rate, Sprite sprite, bool continuouse)
         {
             _frameWidth = sprite.sourceRect.Width / (int)numFrames.X;
             _frameHeight = sprite.sourceRect.Height / (int)numFrames.Y;
-            _currentFrame = new Vector2(1, 1);
+            _startFrame = new Vector2(1, 1);
+            _currentFrame = _startFrame;
             _numFrames = numFrames;
             _switchFrame = rate;
             _frameCounter = 0;
             _SetRect();
+            _continuouse = false;
         }
         #endregion
 
@@ -59,13 +67,35 @@ namespace XNAGameEngine
         #region Private Helper Functions
         private void _NextFrame()
         {
-            _currentFrame.X++;
-            if (_currentFrame.X >= _numFrames.X)
+            if (_continuouse)
             {
-                _currentFrame.X = 0;
-                _currentFrame.Y++;
-                if (_currentFrame.Y >= _numFrames.Y)
-                    _currentFrame.Y = 0;
+                if (_currentFrame != _endFrame)
+                {
+                    _currentFrame.X++;
+                    if (_currentFrame.X >= _numFrames.X)
+                    {
+                        _currentFrame.X = 0;
+                        _currentFrame.Y++;
+                        if (_currentFrame.Y >= _numFrames.Y)
+                            _currentFrame.Y = 0;
+                    }
+                }
+                else
+                    _currentFrame = _startFrame;
+            }
+            else
+            {
+                if (_currentFrame != _endFrame)
+                {
+                    _currentFrame.X++;
+                    if (_currentFrame.X >= _numFrames.X)
+                    {
+                        _currentFrame.X = 0;
+                        _currentFrame.Y++;
+                        if (_currentFrame.Y >= _numFrames.Y)
+                            _currentFrame.Y = 0;
+                    }
+                }
             }
         }
 
